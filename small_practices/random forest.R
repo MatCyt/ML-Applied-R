49765ed27c7b08d9156e75cc927dabd996faa8dc
MatCyt <mateusz.cytrowski@gmail.com>
2019-04-07 17:23:50 +0200

    random forest practice with CARET

--- small_practices/random forest.R
+++ small_practices/random forest.R
@@ -10,6 +10,10 @@
 # libraries
 library(caret)
 library(rpart.plot) # to visualize
+library(mlbench)
+
+# data
+data(Sonar)
 
 set.seed(123)
 
@@ -21,9 +25,9 @@ data_test = Sonar[-trainIndex,]
 trctrl = trainControl(method = "cv", number = 10, repeats = 3)
 
 tree_fit = train(Class ~., data = data_train, method = "rpart",
-                   parms = list(split = "information"),
-                   trControl=trctrl,
-                   tuneLength = 10)
+                 parms = list(split = "information"),
+                 trControl=trctrl,
+                 tuneLength = 10)
 
 tree_fit
 
@@ -37,8 +41,62 @@ confusionMatrix(as.factor(p), data_test$Class)
 
 set.seed(12)
 model = train(Class ~ .,
-               data = Sonar,
-               method = "ranger") 
+              data = Sonar,
+              method = "ranger") 
 print(model)
 
+plot(model)
+
+
+#### mtry
+# caret automate the tuning of the hyperparameter using a grid search, which can be parametrised 
+# by setting tuneLength (that sets the number of hyperparameter values to test) 
+# or directly defining the tuneGrid (the hyperparameter values)
+
+
+### Tune Length
+# The tuneLength parameter tells the algorithm to try different default values for the main parameter
+
+model_tl5 = train(Class ~ .,
+                  data = Sonar,
+                  method = "ranger",
+                  tuneLength = 5)
+
+print(model_tl5)
+
+model_tl2 = train(Class ~ .,
+                  data = Sonar,
+                  method = "ranger",
+                  tuneLength = 2)
+
+print(model_tl2)
+
+### tuneGrid
+# The tuneGrid parameter lets us decide which values the main parameter will take
+# While tuneLength only limit the number of default parameters to use.
+
+set.seed(42)
+myGrid = expand.grid(mtry = c(5, 10, 20, 40, 60),
+                     splitrule = c("gini", "extratrees"),
+                     min.node.size = 5)
+
+model_tG = train(Class ~ .,
+                 data = Sonar,
+                 method = "ranger", 
+                 tuneGrid = myGrid,
+                 trControl = trainControl(method = "cv",
+                                          number = 5,
+                                          verboseIter = FALSE))
+print(model_tG)
+
+# Random forest, 5-fold cross validation, tuneLength = 5
+
+set.seed(42)
+model = train(Class ~ .,
+               data = Sonar,
+               method = "ranger",
+               tuneLength = 5,
+               trControl = trainControl(method = "cv",
+                                        number = 5,
+                                        verboseIter = FALSE))
 plot(model)
