a8d09205fd65668e7dadb20966810ff3588f7d75
Mateusz Cytrowski <mateusz.cytrowski@lingarogroup.com>
2019-04-11 09:24:29 +0200

    .

--- Titanic/titanic_submission_test_caret.R
+++ Titanic/titanic_submission_test_caret.R
@@ -19,18 +19,33 @@ str(full)
 summary(full)
 
 
-
 ### simple data preprocessing
 
 # change into factor
+factor_vars = c('Pclass','Sex','Embarked')
+full[factor_vars] = lapply(full[factor_vars], function(x) as.factor(x))
 
-# simple imputation of NAs in age
+# simple imputation of NAs in age with a mean
+full$Age[is.na(full$Age)] = mean(full$Age, na.rm = T)
 
-# simple imputation of NAs in Fare 
+# simple imputation of NAs in Fare - median
+full[is.na(full$Fare),] # 1 row
+
+full$Fare[is.na(full$Fare)] = median(full$Fare, na.rm = T)
 
 # simple imputation of NAs in Embarked
+sum(is.na(full$Embarked))
+full[is.na(full$Embarked), ]
 
+table(full$Embarked, full$Pclass)
 
+full$Embarked[is.na(full$Embarked)] = "S"
 
 ### run several models for comparison
+fdfs
 
+
+### small tuning
+
+
+### predict and save results
